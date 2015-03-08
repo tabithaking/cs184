882a54f02c938dce01ea42072c7c53a3df80e42f
clairethompson <cthompson44@berkeley.edu>
2015-03-08 15:19:26 -0700

    update light class

--- light.cpp
+++ light.cpp
@@ -1,32 +1,31 @@
 #include "vector.h"
 #include "color.h"
 
-struct Light
+class Light
 {
-	Vector v;
+	Point p;
 	Color intensity;
 	float falloff = 0;
+
+	Light newAmbientLight(float r, float g, float b) {
+		Light l;
+		l.v = NULL;
+		l.intensity = Color(r, g, b);
+		return l;
+	}
+
+	Light newPointLight(float x, float y, float z, float r, float g, float b, float falloff) {
+		Light l;
+		l.v = Vector(x, y, z);
+		l.intensity = Color(r, g, b);
+		l.falloff = falloff;
+		return l;
+	}
+
+	Light newDirectLight(float x, float y, float z, float r, float g, float b) {
+		Light l;
+		l.v = Vector(0.0 - x, 0.0 - y, 0.0 - z);
+		l.intensity = Color(r, g, b);
+		return l;
+	}
 };
-
-Light newAmbientLight(float r, float g, float b) {
-	Light l;
-	l.v = NULL;
-	l.intensity = newColor(r, g, b);
-	return l;
-}
-
-Light newPointLight(float x, float y, float z, float r, float g, float b, float falloff) {
-	Light l;
-	l.v = newVector(x, y, z);
-	l.intensity = newColor(r, g, b);
-	l.falloff = falloff;
-	return l;
-}
-
-Light newDirectLight(float x, float y, float z, float r, float g, float b) {
-	Light l;
-	l.v = newVector(0.0 - x, 0.0 - y, 0.0 - z);
-	l.intensity = newColor(r, g, b);
-	l.falloff = falloff;
-	return l;
-}
